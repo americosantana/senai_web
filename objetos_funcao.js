b590b0d340c45b050061ed4d33b23fb1ee75be1f
Jose Americo <americosantana@gmail.com>
2019-08-15 17:21:48 -0300

    novos arqvuivos adicionados 17:21

---
+++ objetos_funcao.js
@@ -0,0 +1,37 @@
+/*const produto = {}
+
+produto.nome = 'celular'
+produto.preco = 10000
+produto.cor = 'azul'
+
+console.log(produto.nome, produto.preco, produto.cor)
+
+produto.categoria = {tipo: 'smartphone', loja: 'americanas'}
+
+console.log(produto)
+
+const objeto = {
+    item1: 'Valor do item',
+    item2: ' '
+}
+
+console.log(objeto)
+
+const objetoestranho = {
+    ['nome^ Estranho'] : 0
+}
+
+console.log(objetoestranho) */
+
+function imprimir_soma(n1, n2){
+console.log(n1*n2);
+}
+imprimir_soma(2, 8, 10, 15)
+
+function multiplar(n1, n2){
+return n1*n2
+}
+
+console.log(multiplar(2,2))
+
+console.log(multiplar())
