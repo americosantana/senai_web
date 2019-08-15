39a9b0bad4e65e17781f0a974e3655514e6686d5
Jose Americo <americosantana@gmail.com>
2019-08-15 15:44:44 -0300

    adicionado novos arquivos js

---
+++ string.js
@@ -0,0 +1,14 @@
+const escola = 'SENAI'
+
+console.log(escola.charAt(1))
+console.log(escola.charAt(4))
+console.log(escola.charAt(6))
+
+console.log(escola.charCodeAt(2))
+console.log(escola.indexOf('I'))
+
+console.log(escola.substring(2))
+
+//console.log('escola ' .concat(escola) .concat(' - SERVIÇO NACIONAL DE APRENDIZAGEM INDUSTRIAL'))
+
+console.log('Segunda, Terça, Quarta' .split(','))
---
+++ tipo_fraco.js
@@ -0,0 +1,11 @@
+let teste = 'dia legal'
+
+console.log(teste)
+teste = 15
+console.log(teste)
+
+console.log(typeof teste)
+
+
+
+console.log(typeof teste)
