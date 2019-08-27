775a51e72bed5e8dad25208be0569f202ecb3770
Jose Americo <americosantana@gmail.com>
2019-08-27 16:57:32 -0300

    arquivo de formulário modificado

--- formulario/servidor.js
+++ formulario/servidor.js
@@ -2,3 +2,21 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const filtro = require ('./lista')
+const bodyParser = require('body-parser')
+
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With, Content-Type, Acept');
+    next();
+})
+
+//função para acesso dos filtros
+app.get('/', function(req, res, next){
+    res.send(filtro.resultado);
+})
+
+//configuração de funcionamento do app na porta 3003, configure linha 1
+app.listen(porta, () =>{
+    console.log(`Servidor executando na porta ${porta}.`)
+})
